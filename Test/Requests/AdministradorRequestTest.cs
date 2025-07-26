f2d74f085a63cabae7dbc7d5f5866a2e94735b81
fabiosoares-silva <fabiosores.soares@gmail.com>
2025-07-26 00:22:16 -0300

    Incluir mock e teste request

--- Test/Requests/AdministradorRequestTest.cs
+++ Test/Requests/AdministradorRequestTest.cs
@@ -1,13 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MinimalApiBancoDeDadosETestes.Dominio.DTOs;
+using MinimalApiBancoDeDadosETestes.Dominio.ModelViews;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
+using Test.Helpers;
 
 namespace Test.Requests
 {
     public class AdministradorRequestTest
     {
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            Setup.ClassInit(testContext);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Setup.ClassCleanup();
+        }
+
+        [TestMethod]
+        public async Task TestarGetSetPropriedades()
+        {
+            // Arrange
+            var loginDTO = new LoginDTO
+            {
+                Email = "adm@teste.com",
+                Senha = "123456"
+            };
+
+            var content = new StringContent(JsonSerializer.Serialize(loginDTO), Encoding.UTF8, "Application/json");
+
+            // Act
+            var response = await Setup.client.PostAsync("/administradores/login", content);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            var result = await response.Content.ReadAsStringAsync();
+            var admLogado = JsonSerializer.Deserialize<AdministradorLogado>(result, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            Assert.IsNotNull(admLogado?.Email ?? "");
+            Assert.IsNotNull(admLogado?.Perfil ?? "");
+            Assert.IsNotNull(admLogado?.Token ?? "");
+
+            Console.WriteLine(admLogado?.Token);
+        }
 
     }
 }
